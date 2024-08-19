878f40eeee2efb326eb14a0c79c9097d199d5266
onghuilam <onghuilam@gmail.com>
2024-08-19 14:24:03 +0800

    added more constant

--- src/main/java/hl/plugin/image/IMLDetectionPlugin.java
+++ src/main/java/hl/plugin/image/IMLDetectionPlugin.java
@@ -10,7 +10,10 @@ import hl.plugin.IBasePlugin;
 public interface IMLDetectionPlugin extends IBasePlugin {
 	
 	public static final String _KEY_MAT_OUTPUT 			= "mat_output";
-	public static final String _KEY_DETECTION_THRESHOLD = "threshold_detection";
+	public static final String _KEY_TOTAL_DETECTION		= "total_detection_count";
+	public static final String _KEY_THRESHOLD_DETECTION = "threshold_detection";
+	public static final String _KEY_THRESHOLD_NMS 		= "threshold_nms";
+	//
 	public static final String _KEY_MATCHING_THRESHOLD 	= "threshold_matching";
 
 	abstract public Map<String,Object> detect(Mat aImageFile, JSONObject aCustomThresholdJson);
