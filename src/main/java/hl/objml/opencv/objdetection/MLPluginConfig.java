3bc3c6dab8fdfe6a38719b0f42aabe2eb677952a
onghuilam <onghuilam@gmail.com>
2025-04-11 10:00:09 +0800

    remove deprecated v1 objml packages

--- src/main/java/hl/objml/opencv/objdetection/MLPluginConfig.java
+++
@@ -1,5 +0,0 @@
-package hl.objml.opencv.objdetection;
-
-@Deprecated
-public class MLPluginConfig extends hl.objml2.plugin.MLPluginConfig {    
-}
--- src/main/java/hl/objml/opencv/objdetection/MLPluginMgr.java
+++
@@ -1,5 +0,0 @@
-package hl.objml.opencv.objdetection;
-
-@Deprecated
-public class MLPluginMgr extends hl.objml2.plugin.MLPluginMgr {
-}
